c1c9f96c2047047a653003923c8624ee88ec1c5a
Miguel Caberta <caber_t@hotmail.com>
2019-03-11 11:16:09 +0100

    Initial Commit

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export default{
+    appName: 'Platzimusic',
+    apiKey: '6cf23541686f4af27a0639c770e9c560',
+    secret: '0d0a23a93914ab4f25f1ddf494542e70',
+    registeredTo: 'Mcaberta'
+}
+
+
+
+// Application name	Platzimusic
+// API key	6cf23541686f4af27a0639c770e9c560
+// Shared secret	0d0a23a93914ab4f25f1ddf494542e70
+// Registered to	Mcaberta
